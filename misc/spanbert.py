827b796b5914067d50cb8fb53a5b6627dc31f2cf
Ananya Hari Narain <40917852+AnanyaHN27@users.noreply.github.com>
2022-03-24 10:35:47 +0000

    Update spanbert.py

--- misc/spanbert.py
+++ misc/spanbert.py
@@ -20,22 +20,25 @@ Original file is located at
 
 import pandas as pd
 import csv
-reference_results = pd.read_csv('/content/drive/MyDrive/Colab Notebooks/pickled_for_colab.csv')
-reference_results.head(1)
+
 
 import torch
 from summarizer import Summarizer
-model = Summarizer()
 from summarizer import Summarizer
-#from summarizer.coreference_handler import CoreferenceHandler
+from summarizer.coreference_handler import CoreferenceHandler
 from transformers import AutoConfig, AutoTokenizer, AutoModel
+
+reference_results = pd.read_csv('/content/drive/MyDrive/Colab Notebooks/pickled_for_colab.csv')
+reference_results.head(1)
+model = Summarizer()
 
 custom_config = AutoConfig.from_pretrained("SpanBERT/spanbert-base-cased")
 custom_config.output_hidden_states=True
 custom_tokenizer = AutoTokenizer.from_pretrained("SpanBERT/spanbert-base-cased")
 custom_model = AutoModel.from_pretrained("SpanBERT/spanbert-base-cased", config=custom_config)
+custom_handler = CoreferenceHandler(greedyness=.5)
 
-spanbert_model = Summarizer(custom_model = custom_model, custom_tokenizer=custom_tokenizer)
+spanbert_model = Summarizer(custom_model = custom_model, custom_tokenizer=custom_tokenizer, sentence_handler=custom_handler)
 
 def spanBert(transcript, num_sentences):
   result = spanbert_model(transcript, min_length=60, num_sentences=num_sentences)
@@ -49,10 +52,6 @@ for i in range(transcripts.shape[0]):
   new_df.append([summ])
   if (end%50 == 0):
     df = pd.DataFrame(new_df,columns=['trans'])
-
     name = "span_bert" + "_" + str(i) + ".csv"
-    
     df.to_csv('/content/drive/MyDrive/Colab Notebooks/' + name)
   end += 1
-
-#transcripts['spanBert_t5'] = transcripts.apply(lambda row: spanBert(row['transcript'], 15), axis=1)

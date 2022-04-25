8b97ade829991ab66361cd4036eda32c19bb3376
Ananya Hari Narain <40917852+AnanyaHN27@users.noreply.github.com>
2022-04-25 20:15:39 +0100

    Update custom_truncation.py

--- bart/custom_truncation.py
+++ bart/custom_truncation.py
@@ -13,13 +13,16 @@ for trans in trans_lst:
   enc_lst = tokenizer.encode(trans)
   if len(enc_lst) > 512:
 
+    ##truncating after the last full stop in the first 256 characters
     first_half = enc_lst[:256].split(".")
     first_half = (".".join(first_half) + ".") if first_half[-1][-1] in [".", "!"] else (".".join(first_half[:-1]) + ".")
 
+    ##truncating after the first full stop in the first 256 characters
     second_half = enc_lst[len(samp)-256:].split(".")
     second_half = (".".join(second_half)) if second_half[0][0].isupper() else ".".join(second_half[1:])
     
     truncated = first_half + second_half
     encoded_final_lst.append([truncated])
 
+##writing to file
 encoded_final_lst.to_csv("/data/custom_truncation.csv")

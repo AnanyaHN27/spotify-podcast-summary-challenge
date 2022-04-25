c031fb4ed1f323b0e7b0dca6bb74a3f709471537
Ananya Hari Narain <40917852+AnanyaHN27@users.noreply.github.com>
2022-04-25 22:21:40 +0100

    Comment

--- preprocessing/bart_train_regex.py
+++ preprocessing/bart_train_regex.py
@@ -4,20 +4,16 @@ import spacy
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
+#Get the data from the .db file
 cnx = sqlite3.connect('podcast_test_set.db')
 df = pd.read_sql_query("SELECT * FROM dataset_", cnx)
 df = df[['episode_id', 'transcript']]
 
+#Get the metadata
 df_metadata = pd.read_csv('metadata.tsv', sep='\t')
-#final_bart_data = pd.merge(df, df_metadata, on='episode_uri', how='inner')
 
+#Join data properly and write to file
 final_bart_data = pd.merge(df, df_metadata, left_on=  ['episode_id'],
                    right_on= ['episode_uri'], 
                    how = 'inner')[['episode_id', 'transcript', 'episode_description']]
-
-print(final_bart_data.iloc[0]['episode_id'])
-print(final_bart_data.iloc[0]['episode_description'])
-
-
 final_bart_data.to_pickle("bart_train_regex.pkl")
-
